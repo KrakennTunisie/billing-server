aaf3e8057c433b2e9b1561e49fb13cfded07e421
oumaima <oumayma.chelly@kouka.io>
2026-03-04 11:58:13 +0100

    Update BillingServiceApplicationTests.java

--- src/test/java/com/example/billingservice/BillingServiceApplicationTests.java
+++ src/test/java/com/example/billingservice/BillingServiceApplicationTests.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.config.import=optional:configserver:",
+    "spring.cloud.config.enabled=false"
+})
 class BillingServiceApplicationTests {
 
     @Test
