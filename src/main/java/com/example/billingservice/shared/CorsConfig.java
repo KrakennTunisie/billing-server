288c5b090c51a0623e50ad8fc04fb4e4eeb4c3af
OumaimaKouka <oumayma.chelly@kouka.io>
2026-04-16 23:53:06 +0200

    fix : fixing cors problem

--- src/main/java/com/example/billingservice/shared/CorsConfig.java
+++ src/main/java/com/example/billingservice/shared/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://89.167.12.94:3000",
+                                "http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
